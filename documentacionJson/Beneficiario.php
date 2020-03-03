193d27ff8470222a7e53c97fc77f8156e1bb8511
carlos <cep11p@gmail.com>
2020-03-03 12:56:43 -0300

    TG-19 - TG-50 #Closed se realiza la documentacionJson para registrar un beneficiario https://tree.taiga.io/project/cep11p-rn-nutre/us/19?no-milestone=1

---
+++ documentacionJson/Beneficiario.php
@@ -0,0 +1,30 @@
+<?php
+
+/**** registrar un beneficiario ***
+@url ejemplo http://api-rnnutre.local/beneficiarios
+@Method POST
+{    
+    "nombre": "Carlos",
+    "apellido": "Pèrez",
+    "nro_documento": "36849868",
+    "fecha_nacimiento":"1992-05-07",
+    "telefono": "2920430690",
+    "celular": "2920412127",
+    "situacion_laboralid": 1,
+    "estado_civilid": 1,
+    "sexoid": 1,
+    "tipo_documentoid": 1,
+    "generoid": 1,
+    "email":"carlos@correo.com.ar",
+    "cuil":"20367655678",
+    "lugar": {
+        "barrio":"Ina lauquen",
+        "calle":"Mata negra",
+        "altura":"327",
+        "piso":"",
+        "depto":"",
+        "escalera":"",
+        "localidadid":1
+    }    
+}
+**/
