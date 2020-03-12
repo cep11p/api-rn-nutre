de0dd187b865e328e3588a5d7f86f1dd9b7a28e0
carlos <cep11p@gmail.com>
2020-03-12 13:09:56 -0300

    TG-71 - TG-80 #Closed Se realiza la documentacion Json https://tree.taiga.io/project/cep11p-rn-nutre/us/71?no-milestone=1

--- documentacionJson/Beneficiario.php
+++ documentacionJson/Beneficiario.php
@@ -36,3 +36,63 @@
     }
 }
 **/
+
+/**
+/**
+*** Para mostrar listado **** 
+Obtenemos una listado de beneficiario, vinculando una persona del sistema registral(Interoperabilidad).
+Podemos aplicar un filtrado aplicando un "?" despues de la url, y concatenadamente le tiramos los parametros relevantes al criterio de busqueda. Estos
+parametros son:
+    page (numero de la pagina, por defecto va el 0 como primera pagina)
+    global_param (busca por nombre, apellido y/o apodo)
+    calle (busca el nombre de la calle donde el beneficiario vive)
+    localidadid (id de la localidad donde el beneficiario vive)
+    estado, etc (cualquier atributo de beneficiario)
+
+La Url quedaria algo asi, (no importa el orden) api/beneficiarios?page=1&global_param=gonzalez&localidadid=1&estado=pendiente
+@url http://api.pril.local/api/beneficiarios
+@method GET
+@return
+{
+    "pagesize": 20,
+    "pages": 1,
+    "total_filtrado": 3,
+    "success": true,
+    "resultado": [
+        {
+            "id": 20,
+            "personaid": 99,
+            "estado": "admitido",
+            "cantidad_hijo": 1,
+            "edad_por_hijo": "22",
+            "persona": {
+                "id": 99,
+                "nombre": "Dora Otilia",
+                "apellido": "Medina",
+                "apodo": "",
+                "nro_documento": "29898483",
+                "fecha_nacimiento": "1987-09-13",
+                "estado_civilid": 3,
+                "telefono": "2920430098",
+                "celular": "2919612299",
+                "sexoid": 1,
+                "tipo_documentoid": 3,
+                "nucleoid": 99,
+                "situacion_laboralid": 3,
+                "generoid": 1,
+                "email": "email22@correo.com",
+                "cuil": "31298984835",
+                "estudios": [],
+                "lista_red_social": [],
+                "sexo": "Masculino",
+                "genero": "Hombre",
+                "lista_oficio": [],
+                "estado_civil": "Divorciado/a",
+                "ultimo_estudio": ""
+            }
+        },
+        {...},
+        {...}
+    ]
+}
+ */
